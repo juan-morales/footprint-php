cadfbef031a99e4022b1883b72fba1edf559a6f6
jcm <juan.carlos.morales@tradebyte.com>
2021-01-11 16:33:39 +0100

    MOdule ChartJS beta 0.1 test full report

--- test/ChartJSModuleTest.php
+++ test/ChartJSModuleTest.php
@@ -4,20 +4,25 @@ use PHPUnit\Framework\TestCase;
 use Footprint\Tracker;
 use Footprint\Modules\ChartJS;
 use Footprint\Modules\Memory;
+use Footprint\Modules\Time;
 
 class ChartsJSModuleTest extends TestCase 
 {
     public function testModule() {
         $mem = str_repeat("X", 1024 * 1024);
-
+        
+        $moduleTime = new Time();
         $moduleMem = new Memory();
 
         $module = new ChartJS();
         $module->addKey("MEMORY_memory_usage_bytes");
         $module->addKey("MEMORY_memory_peak_bytes");
+        $module->addKey("TIME_total_seconds");
+        $module->addKey("TIME_step_seconds");
 
         $tracker = new Tracker();
         $tracker->loadModule($moduleMem);
+        $tracker->loadModule($moduleTime);
         $tracker->loadModule($module);
         $tracker->init();
         $tracker->log();
